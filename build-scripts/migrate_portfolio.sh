f3da7c65650d5d941200b96e5dd8941822a2f342
7iquid <taviefalcon@gmail.com>
2023-11-29 21:34:46 +0800

    Update portfolio From nuxt

---
+++ build-scripts/migrate_portfolio.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -d '.portfolio' ]; then
+  echo 'creating: folder .portfolio'
+  mkdir .portfolio && cd .portfolio
+  git clone https://github.com/7iquid/7iquid.github.io.git
+  cd 7iquid.github.io
+else
+  cd 7iquid.github.io && git pull origin main
+fi
+
+cp -r ../.output/public/* .
+if [[ -n $(git diff --exit-code) ]]; then
+  echo 'git push: folder .portfolio'
+  git add . && git commit -m 'Update portfolio From nuxt' && git push -u origin main
+else
+  echo "No changes to commit. Skipping git push."
+fi
